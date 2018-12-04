ef07e138abef820a4e3399adf97d413d906cacf2
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2018-12-03 21:36:02 -0500

    refactor: some organisation

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -5,7 +5,6 @@ import { Switch } from "react-router";
 import routes from "./routes";
 
 export default function App() {
-    console.log(routes.describe());
     return (
         <HashRouter>
             <Switch>{routes.render()}</Switch>
