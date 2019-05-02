e2c9c80bc740d3ed25654b728c7af21a9ab853a6
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-05-01 23:45:57 -0400

    use dropdown to select experiment

--- src/containers/Labs/Labs.styles.js
+++ src/containers/Labs/Labs.styles.js
@@ -4,12 +4,24 @@ import { Grid, Cell } from "styled-css-grid";
 export const FullGrid = styled(Grid)`
     height: 100%;
     width: 100%;
+    min-height: 480px;
 `;
 
 export const CenteredCell = styled(Cell)`
     display: flex;
+    position: relative;
     text-align: center;
     align-items: center;
     justify-content: center;
     justify-self: stretch;
+`;
+
+const optionFontSize = "1.2em";
+export const Option = styled.option``;
+export const Select = styled.select`
+    font-size: ${optionFontSize};
+
+    ${Option} {
+        font-size: ${optionFontSize};
+    }
 `;
--- src/containers/Labs/index.js
+++ src/containers/Labs/index.js
@@ -5,8 +5,7 @@ import { Beaker } from "styled-icons/octicons";
 import { TestTube } from "styled-icons/boxicons-regular";
 import { Colors, Zindex } from "utils/constants";
 import GameOfLife from "components/GameOfLife";
-import Link from "components/Link";
-import { FullGrid, CenteredCell } from "./Labs.styles";
+import { FullGrid, CenteredCell, Option, Select } from "./Labs.styles";
 import EXPERIMENTS from "./experiments.json";
 
 const xTo = (path, xSlug) => `${path.replace("/:experiment", "")}/${xSlug}`;
@@ -17,102 +16,72 @@ const xName = xSlug =>
     xSlug.substr(0, 1).toUpperCase() +
     xSlug.substr(1).replace(/-(\w)/g, ([, m]) => ` ${m.toUpperCase()}`);
 
-const Experiment = ({ experiment }) => (
-    <iframe
-        title={experiment}
-        width="100%"
-        height="100%"
-        src={xUrl(experiment)}
-        frame-ancestors="ogbizi.com github.com"
-        style={{ border: "none", backgroundColor: "white" }}
-    />
+const BrewingIcon = () => (
+    <CenteredCell height={0} style={{ zIndex: Zindex.TOP }}>
+        <Beaker color={Colors.LIGHT} size="10vmax" />
+        <TestTube
+            color={Colors.LIGHT}
+            size="6vmax"
+            style={{
+                transform: "rotate(-135deg)",
+                position: "absolute",
+                top: "calc(50% - 12vmax)",
+                left: "2vmax",
+            }}
+        />
+    </CenteredCell>
 );
 
+const Experiment = ({ experiment }) => {
+    if (!experiment) return <BrewingIcon />;
+    return (
+        <iframe
+            title={experiment}
+            width="100%"
+            height="100%"
+            src={xUrl(experiment)}
+            frame-ancestors="ogbizi.com github.com"
+            style={{ border: "none", backgroundColor: "white" }}
+        />
+    );
+};
+
 Experiment.propTypes = {
-    experiment: PropTypes.string.isRequired,
+    experiment: PropTypes.string,
+};
+
+Experiment.defaultProps = {
+    experiment: null,
 };
 
 export default function({
+    history,
     match: {
         path,
         params: { experiment },
     },
 }) {
-    const linkText = content => {
-        const style = { flexGrow: 1 };
-        return <span style={style}>{content}</span>;
-    };
-    const brewingIcon = active => {
-        const size = "24px";
-        return (
-            <Beaker
-                color={active ? Colors.ACTIVE : Colors.DARK}
-                size={size}
-                style={{
-                    justifySelf: "flex-end",
-                    minWidth: size,
-                    minHeight: size,
-                    margin: "16px",
-                }}
-            />
-        );
-    };
-    const emptyLabIcon = (
-        <TestTube
-            color={Colors.LIGHT}
-            size="10vmax"
-            style={{ zIndex: Zindex.TOP }}
-        />
-    );
     return [
         <Helmet key="lab-helmet">
             <title>{experiment ? `${xName(experiment)}` : "Laboratory"}</title>
         </Helmet>,
-        <GameOfLife key="game-of-life" fixed background />,
-        <FullGrid key="lab-grid">
-            <CenteredCell width={4} fitContent>
-                <FullGrid
-                    columns={1}
-                    gap="0"
-                    style={{ maxWidth: "300px", minWidth: "240px" }}
+        <GameOfLife key="game-of-life" fixed background darkMode />,
+        <FullGrid key="lab-grid" rows={12} gap="0">
+            <CenteredCell width={12} height={1} fitContent>
+                <Select
+                    onChange={e => history.push(e.target.value)}
+                    defaultValue={xTo(path, experiment)}
                 >
-                    {Object.keys(EXPERIMENTS).map(x => {
-                        const active = x === experiment;
-                        const linkProps = {
-                            backgroundColor: active
-                                ? Colors.LIGHT
-                                : "rgba(255,255,255,0.9)",
-                            borderColor: Colors.NONE,
-                            borderWidth: "0",
-                            buttonWidth: "100%",
-                            buttonHeight: "100%",
-                            fontSize: "24px",
-                            fontColors: {
-                                initial: Colors.DARK,
-                                hover: Colors.ACTIVE,
-                                active: Colors.ACTIVE,
-                            },
-                            to: xTo(path, x),
-                            width: "100%",
-                            height: "100%",
-                        };
-                        return (
-                            <CenteredCell key={x} width={1}>
-                                <Link {...linkProps} active={active}>
-                                    {linkText(xName(x))}
-                                    {!EXPERIMENTS[x] && brewingIcon(active)}
-                                </Link>
-                            </CenteredCell>
-                        );
-                    })}
-                </FullGrid>
+                    <Option value="/labs">Choose an experiment</Option>
+                    {Object.keys(EXPERIMENTS).map(x => (
+                        <Option key={x} value={xTo(path, x)}>
+                            {xName(x)}
+                        </Option>
+                    ))}
+                </Select>
             </CenteredCell>
-            <CenteredCell width={8}>
-                {experiment ? (
-                    <Experiment experiment={experiment} />
-                ) : (
-                    emptyLabIcon
-                )}
+            <CenteredCell width={12} height={11}>
+                <Experiment experiment={experiment} />
             </CenteredCell>
         </FullGrid>,
     ];
