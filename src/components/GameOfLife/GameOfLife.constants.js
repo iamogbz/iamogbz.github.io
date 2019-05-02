a1ef6d881463290ea6056f1d2c0b6adfb2b13663
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-05-01 22:30:19 -0400

    dark and light mode for GOL

--- src/components/GameOfLife/GameOfLife.constants.js
+++ src/components/GameOfLife/GameOfLife.constants.js
@@ -1,6 +1,8 @@
 export const BOARD_ID = "game-of-life-board";
-export const BOARD_COLOR = "#000";
-export const GRID_COLOR = "#111";
+export const BOARD_COLOR_LIGHT = "#fff";
+export const GRID_COLOR_LIGHT = "#eee";
+export const BOARD_COLOR_DARK = "#000";
+export const GRID_COLOR_DARK = "#111";
 export const CELL_COLOR = "#333";
 export const CELL_BORDER = 1;
 export const CELL_SIZE = 20;
