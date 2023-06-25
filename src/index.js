d725589a6a95ba8c13e4aed3e2f660311dff7f86
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2023-06-25 09:33:07 -0400

    refactor: move component import to index script tag

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 // eslint-disable-next-line no-console
 console.log("Hello Curious 👋🏾😸 https://github.com/iamogbz/iamogbz.github.io");
-
+// index.html components
+import "./components/button/animated-button.js";
+import "./components/logo/animated-logo.js";
+// index.html script
+import { compareRepos, fetchRepos } from "./utils/github/repos.js";
 import { includeTemplate } from "./utils/include-template.js";
 
 includeTemplate({
