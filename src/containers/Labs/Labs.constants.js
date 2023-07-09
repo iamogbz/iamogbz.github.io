5ad4a03b4c275ec557c7650ef8fa1c9d2f2f2dc7
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2023-07-09 00:47:13 +0000

    patch: hide some projects
    site and jest-react-snapshot

--- src/containers/Labs/Labs.constants.js
+++ src/containers/Labs/Labs.constants.js
@@ -1,4 +1,7 @@
 export { GRAPH_QUERY } from "components/GithubLanguages/GithubLanguages.constants";
 
-export const PROJ_BLACKLIST = [];
+export const PROJ_BLACKLIST = [
+  "iamogbz.github.io",
+  "jest-react-snapshot",
+];
 export const CLS_PROJECT_SELECTED = "selected";
