0cf780fa9001d6433c2a2c62764f6f3a4d086ebd
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2025-06-17 09:13:53 -0400

    fix: replace github graph ql client with function hook

--- src/components/GithubLanguages/index.js
+++ src/components/GithubLanguages/index.js
@@ -2,41 +2,35 @@ import React from "react";
 import PropTypes from "prop-types";
 import get from "lodash/get";
 import { Colors } from "utils/constants";
-import GithubGraphQLClient from "services/Github/GraphQLClient";
+import { useGraphQuery } from "services/Github/useGraphQuery";
 import LanguageBadge from "components/LanguageBadge";
 import { LANG_BLACKLIST, GRAPH_QUERY } from "./GithubLanguages.constants";
 
 const getUserUrl = user => `https://github.com/${user.toLowerCase()}`;
 
-export default class GithubLanguages extends React.PureComponent {
-    constructor(props) {
-        super(props);
-        const { authKey } = this.props;
-        this.state = {};
-        this.client = GithubGraphQLClient({
-            headers: {
-                Authorization: `Bearer ${authKey}`,
-            },
-        });
-        this.client
-            .request(GRAPH_QUERY, {
-                repoCount: 100,
-                langCount: 10,
-            })
-            .then(data => this.setState({ data }));
-    }
+function GithubLanguages({ authKey }) {
+    const data = useGraphQuery({
+        authKey,
+        graphQuery: GRAPH_QUERY,
+        queryParams: {
+            repoCount: 100,
+            langCount: 10,
+        },
+    });
 
-    get getLanguageReposUrl() {
-        const { data } = this.state;
-        const userUrl = getUserUrl(get(data, "viewer.login", "404"));
-        return lang =>
-            `${userUrl}?tab=repositories&language=${lang
+    const getLanguageReposUrl = React.useCallback(
+        lang => {
+            const userUrl = getUserUrl(get(data, "viewer.login", "404"));
+            return `${userUrl}?tab=repositories&language=${lang
                 .replace(" ", "+")
                 .toLowerCase()}`;
-    }
+        },
+        [data],
+    );
 
-    get languageDistribution() {
-        const { data } = this.state;
+    const languageDistribution = React.useMemo(() => {
+        if (!data) return { total: 0, types: {} };
+
         const langs = { total: 0, types: {} };
         get(data, "viewer.repositories.nodes", []).forEach(repo => {
             get(repo, "languages.edges", []).forEach(lang => {
@@ -47,7 +41,7 @@ export default class GithubLanguages extends React.PureComponent {
                 if (!langs.types[name]) {
                     langs.types[name] = {
                         size: 0,
-                        url: this.getLanguageReposUrl(name),
+                        url: getLanguageReposUrl(name),
                     };
                 }
                 langs.types[name].size += size;
@@ -55,38 +49,43 @@ export default class GithubLanguages extends React.PureComponent {
             });
         });
         return langs;
+    }, [data, getLanguageReposUrl]);
+
+    if (!data) {
+        return null; // Or a loading indicator
     }
 
-    render() {
-        const { total, types } = this.languageDistribution;
-        const colors = {
-            $: {
-                foreground: Colors.DARK,
-                background: `color-mix(in srgb, ${Colors.LIGHT} 70%, transparent)`,
-            },
-            hover: {
-                background: `color-mix(in srgb, ${Colors.LIGHT} 90%, transparent)`,
-            },
-        };
-        const sortedDists = Object.entries(types).sort(
-            ([, a], [, b]) => b.size - a.size,
+    const { total, types } = languageDistribution;
+    const colors = {
+        $: {
+            foreground: Colors.DARK,
+            background: `color-mix(in srgb, ${Colors.LIGHT} 70%, transparent)`,
+        },
+        hover: {
+            background: `color-mix(in srgb, ${Colors.LIGHT} 90%, transparent)`,
+        },
+    };
+    const sortedDists = Object.entries(types).sort(
+        ([, a], [, b]) => b.size - a.size,
+    );
+
+    return sortedDists.map(([name, { size, url }]) => {
+        const showUrlLink = !LANG_BLACKLIST.includes(name.toLowerCase());
+        return (
+            <LanguageBadge
+                name={name}
+                key={name}
+                numerator={size}
+                denominator={total}
+                url={showUrlLink ? url : null}
+                colors={colors}
+            />
         );
-        return sortedDists.map(([name, { size, url }]) => {
-            const showUrlLink = !LANG_BLACKLIST.includes(name.toLowerCase());
-            return (
-                <LanguageBadge
-                    name={name}
-                    key={name}
-                    numerator={size}
-                    denominator={total}
-                    url={showUrlLink ? url : null}
-                    colors={colors}
-                />
-            );
-        });
-    }
+    });
 }
 
 GithubLanguages.propTypes = {
     authKey: PropTypes.string.isRequired,
 };
+
+export default React.memo(GithubLanguages);
